a069071c9ecf38bf4b72da20607a95d884ecc802
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 21:18:07 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,19 +1,46 @@
 class Genre
-
-  @@all = []
-
-  def self.all
-    @@all
-  end
-  attr_accessor :genres
+  attr_accessor :name
 
   def initialize(name)
-    @@name = name
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.genre = self
   end
 
   def songs
-    Song.all.select{|song| sonng.artist == self}
+    @songs
+  end
+
+  def artists
+    @songs.collect do |song|
+      song.artist
+    end
   end
 
 
-end
+
+
+# class Genre
+# 
+#   @@all = []
+# 
+#   def self.all 
+#     @@all 
+#   end
+#   
+#   attr_accessor :genres
+# 
+#   def initialize(name)
+#     @@name = name
+#   end
+# 
+#   def songs
+#     Song.all.select{|song| sonng.artist == self}
+#   end
+# 
+# 
+# end
