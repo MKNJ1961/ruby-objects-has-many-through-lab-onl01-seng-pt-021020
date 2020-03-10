fbc1ac5f70f367e56e24698927b9bf3da9056f35
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 22:27:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ class Artist
   def initialize(name)
     @name = name
     @genres = []
-    @@all = self
+    @@all << self
   end
   def new_song(name, genre)
     song = Song.new(name, genre, self)
