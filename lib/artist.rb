f273ace4516598f57ccd460696f7cfd2e52411ce
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 22:30:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,11 +21,11 @@ class Artist
   #   genre.songs << songs
   #   song
   # end
-  # 
+  #
   # def songs
   #   Song.all.select{|song| song.artist == self}
   # end
-  # 
+  #
   # def genres
   #   Genre.all.select{|genre| genre.artist == self}
   # end
