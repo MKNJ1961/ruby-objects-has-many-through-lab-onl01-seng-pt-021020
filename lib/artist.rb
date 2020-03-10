1af8f9991fe60eba2cd6fc1cd728fafd0d99bbfd
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 19:20:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-    attr_accessor :name
+    attr_accessor :name, :genre
 
     def self.all
       @@all
@@ -9,6 +9,7 @@ class Artist
 
   def initialize(name)
     @name = name
+    @genre = genre
     @@all << self
   end
 
