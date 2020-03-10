d2d3e34b732d81d0ba57be153e9f99a3a276960b
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 22:31:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,12 +14,12 @@ class Artist
     @@all << self
   end
 
-  def new_song(name, genre)
-    song = Song.new(name, genre, self)
-    genre.artists << self
-    genre.songs << songs
-    song
-  end
+  # def new_song(name, genre)
+  #   song = Song.new(name, genre, self)
+  #   genre.artists << self
+  #   genre.songs << songs
+  #   song
+  # end
   #
   # def songs
   #   Song.all.select{|song| song.artist == self}
