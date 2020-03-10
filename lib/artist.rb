cb15610694cabfc2094a2bea459d548989f12a6b
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 22:28:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,32 @@
-class Artist
-  @@all = []
-  def self.all
-    @@all
-  end
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-    @genres = []
-    @@all << self
-  end
-  def new_song(name, genre)
-    song = Song.new(name, genre, self)
-    genre.artists << self
-    genre.songs << songs
-    song
-  end
-  def songs
-    Song.all.select{|song| song.artist == self}
-  end
-  def genres
-    Genre.all.select{|genre| genre.artist == self}
-  end
-end
+# class Artist
+#   
+#   @@all = []
+#   
+#   def self.all
+#     @@all
+#   end
+#   
+#   attr_accessor :name
+#   
+#   def initialize(name)
+#     @name = name
+#     @genres = []
+#     @@all << self
+#   end
+#   
+#   def new_song(name, genre)
+#     song = Song.new(name, genre, self)
+#     genre.artists << self
+#     genre.songs << songs
+#     song
+#   end
+#   
+#   def songs
+#     Song.all.select{|song| song.artist == self}
+#   end
+#   
+#   def genres
+#     Genre.all.select{|genre| genre.artist == self}
+#   end
+#   
+# end
